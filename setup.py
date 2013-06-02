153d8163440d48e7e6ae65810f0fb129b98c7438
Long Ho <longho@clearchannel.com>
2013-06-02 17:40:20 -0400

    rename README, add requirements.txt and make setup.py read from it

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
+
+req_file = open('requirements.txt', 'r')
+lines = req_file.readlines()
+requirements = [l.strip().strip('\n') for l in lines if l.strip() and not l.strip().startswith('#')]
+
 setup(
     name='pytest-cache',
     description='pytest plugin with mechanisms for caching across test runs',
-    long_description=open("README.txt").read(),
+    long_description=open("README.md").read(),
     version='1.0dev2',
     author='Holger Krekel',
     author_email='holger.krekel@gmail.com',
-    url='http://bitbucket.org/hpk42/pytest-cache/',
+    maintainer='Long Ho',
+    maintainer_email='holevietlong@gmail.com',
+    url='https://github.com/longlho/pytest-cache.git',
     py_modules=['pytest_cache'],
     entry_points={'pytest11': ['cacheprovider = pytest_cache']},
-    install_requires=['pytest>=2.2', 'execnet>=1.1.dev1', ],
+    install_requires=requirements,
     classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
